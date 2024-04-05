63af0ceaeff5936a715a34796b2d369b184904fb
ex.nicolas.gimenez <nicolas.gimenez@externes.justice.gouv.fr>
2024-04-05 10:19:13 +0200

    fix types and exports to allow typescript imports

--- dist/cytoscape-node-html-label.d.ts
+++ dist/cytoscape-node-html-label.d.ts
@@ -1,8 +1,22 @@
 declare type IHAlign = "left" | "center" | "right";
 declare type IVAlign = "top" | "center" | "bottom";
-declare var module: any;
+interface NodeRequireFunction {
+    (id: string): any;
+}
+interface NodeModule {
+    exports: any;
+    require: NodeRequireFunction;
+    id: string;
+    filename: string;
+    loaded: boolean;
+    parent: NodeModule | null | undefined;
+    children: NodeModule[];
+    path: string;
+    paths: string[];
+}
+declare var module: NodeModule;
 declare var define: any;
-declare var cytoscape: any;
+declare var cytoscape: typeof cytoscape;
 interface CytoscapeNodeHtmlParams {
     query?: string;
     halign?: IHAlign;
@@ -15,3 +29,4 @@ interface CytoscapeNodeHtmlParams {
 interface CytoscapeContainerParams {
     enablePointerEvents?: boolean;
 }
+declare module 'cytoscape-node-html-label';
--- src/cytoscape-node-html-label.ts
+++ src/cytoscape-node-html-label.ts
@@ -1,9 +1,32 @@
-type IHAlign = "left" | "center" | "right";
-type IVAlign = "top" | "center" | "bottom";
-declare var module: any;
-declare var define: any;
-declare var cytoscape: any;
+declare type IHAlign = "left" | "center" | "right";
+declare type IVAlign = "top" | "center" | "bottom";
 
+interface NodeRequireFunction {
+  (id: string): any;
+}
+
+interface NodeModule {
+  exports: any;
+  require: NodeRequireFunction;
+  id: string;
+  filename: string;
+  loaded: boolean;
+  /** @deprecated since 12.19.0 Please use `require.main` and `module.children` instead. */
+  parent: NodeModule | null | undefined;
+  children: NodeModule[];
+  /**
+   * @since 11.14.0
+   *
+   * The directory name of the module. This is usually the same as the path.dirname() of the module.id.
+   */
+  path: string;
+  paths: string[];
+}
+
+declare var module: NodeModule;
+declare var define: any;
+// @ts-ignore
+declare var cytoscape: typeof cytoscape;
 interface CytoscapeNodeHtmlParams {
   query?: string;
   halign?: IHAlign;
@@ -13,10 +36,13 @@ interface CytoscapeNodeHtmlParams {
   cssClass?: string;
   tpl?: (d: any) => string;
 }
-
 interface CytoscapeContainerParams {
   enablePointerEvents?: boolean;
 }
+
+declare module 'cytoscape-node-html-label'
+
+
 
 (function () {
   "use strict";
